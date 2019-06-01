8129d677a4542a74c27dbd5de7464fea81443e42
Grant Schroeder <gschroeder@pznma.com>
2019-06-01 01:27:50 +0000

    ready for launch

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,4 @@
 class Quote < ApplicationRecord
+  validates :saying, presence: true, length: {Maximum: 140, minimum: 3 }
+  validates :author, presence: true, length: {Maximum: 50, minimum: 3 }
 end
